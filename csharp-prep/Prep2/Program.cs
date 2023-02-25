314d15467ecb891e5c8ad43df5cc09d10e4280c4
RaeLee Brunt <raelee.riccardo@gmail.com>
2023-02-24 19:09:08 -0700

    Completed Prep 2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userValue = Console.ReadLine();
+        int value = int.Parse(userValue); 
+
+        string letter = "";
+
+        if (value >= 90)
+        {
+            letter = "A";
+        }
+        else if (value >= 80)
+        {
+            letter = "B";
+        }
+        else if (value >= 70)
+        {
+            letter = "C";
+        }
+        else if (value >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Grade: {letter}");
+
+        if (value >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Next time you will get it, keep trying.");
+        }
     }
 }
