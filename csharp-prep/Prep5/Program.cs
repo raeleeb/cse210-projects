b016a49659f43c944006af544613367649215aab
RaeLee Brunt <raelee.riccardo@gmail.com>
2023-02-25 17:11:12 -0700

    Prep 5, complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,51 @@ using System;
 
 class Program
 {
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string num = Console.ReadLine();
+        int number = int.Parse(num);
+
+        return number;
+    }
+
+    static double SquareNumber(double number)
+    {
+        double squareNum = Math.Pow(number,2);
+
+        return squareNum;
+    }
+
+    static void DisplayResult(string name, double number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+
+        int faveNumber = PromptUserNumber();
+
+        double numSquared = SquareNumber(faveNumber);
+
+        DisplayResult(userName, numSquared);
     }
+
 }
