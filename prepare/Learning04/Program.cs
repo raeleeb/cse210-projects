d6997840d6fd1f053eb7cfa82fdcf7a35379bc36
RaeLee Brunt <raelee.riccardo@gmail.com>
2023-03-15 19:59:47 -0600

    Learning 4 - Complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment asst1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(asst1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment asst2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(asst2.GetSummary());
+        Console.WriteLine(asst2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment asst3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(asst3.GetSummary());
+        Console.WriteLine(asst3.GetWritingInformation());
     }
 }
