cb9b5d908f78cfaa967835b65c70931c2ce86121
RaeLee Brunt <raelee.riccardo@gmail.com>
2023-03-22 00:39:13 -0600

    Learning 5 - Complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Yellow", 5);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Purple", 7, 2);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Pink", 2);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+        
+
     }
 }
